fd314c64233f368d283f964909ffaaecce41f04c
jab001 <production@agima.ru>
2021-02-08 15:44:09 +0300

    Исправил ковычки

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./container/App";
-import {FILMS} from "./const";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './container/App';
+import {FILMS} from './const';
 
 ReactDOM.render(<App films={FILMS} />, document.querySelector(`#root`));
